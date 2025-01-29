d00967df026d19d3fcf649802e774ee6c653984d
Daniela Eloisa <danielaeloisa1@gmail.com>
2025-01-29 08:26:49 -0300

    Estilizando menu do ScreenSound e adicionando opções de escolha ao menu.

--- ScreenSound/ScreenSound/Program.cs
+++ ScreenSound/ScreenSound/Program.cs
@@ -2,18 +2,37 @@
 // Screen Sound
 
 
+
 public class Program
 {
     private static void Main(string[] args)
     {
         ExibirMenssagensDeBoasVindas();
+        ExibirOpcoesDoMenu();
+    }
+
+    private static void ExibirOpcoesDoMenu()
+    {
+        Console.WriteLine("Digite 1 para registrar uma banda.");
+        Console.WriteLine("Digite 2 para mostrar todas as bandas.");
+        Console.WriteLine("Digite 3 para avaliar uma banda.");
+        Console.WriteLine("Digite 4 para exibir a média de uma banda.");
+        Console.WriteLine("Digite -1 para sair.");
     }
 
     private static void ExibirMenssagensDeBoasVindas()
     {
-        string mensagemdeBoasVindas = " Boas vindas ao Screen Sound";
-        Console.WriteLine("*****************************");
+        string mensagemdeBoasVindas = @"
+
+ ░██████╗░█████╗░██████╗░███████╗███████╗███╗░░██╗  ░██████╗░█████╗░██╗░░░██╗███╗░░██╗██████╗░
+ ██╔════╝██╔══██╗██╔══██╗██╔════╝██╔════╝████╗░██║  ██╔════╝██╔══██╗██║░░░██║████╗░██║██╔══██╗
+ ╚█████╗░██║░░╚═╝██████╔╝█████╗░░█████╗░░██╔██╗██║  ╚█████╗░██║░░██║██║░░░██║██╔██╗██║██║░░██║
+ ░╚═══██╗██║░░██╗██╔══██╗██╔══╝░░██╔══╝░░██║╚████║  ░╚═══██╗██║░░██║██║░░░██║██║╚████║██║░░██║
+ ██████╔╝╚█████╔╝██║░░██║███████╗███████╗██║░╚███║  ██████╔╝╚█████╔╝╚██████╔╝██║░╚███║██████╔╝
+ ╚═════╝░░╚════╝░╚═╝░░╚═╝╚══════╝╚══════╝╚═╝░░╚══╝  ╚═════╝░░╚════╝░░╚═════╝░╚═╝░░╚══╝╚═════╝░
+";
         Console.WriteLine(mensagemdeBoasVindas);
-        Console.WriteLine("*****************************");
+        Console.WriteLine("Boas vindas ao Screen Sound!\n");
+
     }
 }
